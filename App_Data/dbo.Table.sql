710ae3e9220b2d606ac263880259d3ad6557b983
chris <cla24@njit.edu>
2020-03-28 12:15:48 -0400

    initial

---
+++ App_Data/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]        INT          IDENTITY (1, 1) PRIMARY KEY,
+    [Name]      VARCHAR (50) NULL,
+    [Age]       VARCHAR (50) NULL,
+    [Address]   VARCHAR (50) NULL,
+    [Mobile No] VARCHAR (50) NULL,
+    [Email id]  VARCHAR (50) NULL,
+);
+
